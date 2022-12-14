2afc546fbbcd4cdd102f405b7566bb1d2aee8b6b
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-12-13 19:07:29 -0500

    Added in and styled the majority of the about section

--- portfolio/src/sections/AboutSection.js
+++ portfolio/src/sections/AboutSection.js
@@ -2,7 +2,26 @@ import React from "react";
 import "../styles/aboutsection.css";
 
 const AboutSection = () => {
-  return <div className="about-section"></div>;
+  return (
+    <div className="about-section">
+      <h3>About Me</h3>
+      <p>
+        Hi! My name is Barbara, and I am a frontend developer and UI/UX designer
+        based in south Florida. I have been designing and building web products
+        for over 1 year, and in that time I have produced simple web
+        applications, such as a weather and forecast website, to more advanced
+        web applications that helped increase customers for local businesses. I
+        am a self-taught developer and designer, which indicates my dedication
+        and drive to achieve the results I expect.
+        <br />
+        <br />
+        When I am not on my computer working on my latest project, I enjoy
+        long-distance running, cooking, and reading both fiction and non-fiction
+        books.
+      </p>
+      <h3>My Skills</h3>
+    </div>
+  );
 };
 
 export default AboutSection;
